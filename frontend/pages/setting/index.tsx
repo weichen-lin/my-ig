6f86cf128f2490acc82f5f124461a5fc423641da
WeiChen Lin <asdfg09487@gmail.com>
2023-06-27 00:56:29 +0800

    adjust setting

--- frontend/pages/setting/index.tsx
+++ frontend/pages/setting/index.tsx
@@ -13,7 +13,7 @@ import { LayoutHome } from 'components/layout'
 import { IgProvider } from 'context'
 import { CookieParser, TokenProp } from 'hooks/utils'
 
-import { MdUploadFile } from 'react-icons/md'
+import { MdUploadFile, MdOutlineCheck } from 'react-icons/md'
 
 export default function SettingPage(props: TokenProp) {
   const { token, current } = props
@@ -21,7 +21,7 @@ export default function SettingPage(props: TokenProp) {
   return (
     <LayoutHome>
       <div className='border-2 border-gray-100 rounded-lg p-4 w-[90%] mx-auto flex flex-col'>
-        <p className='text-2xl text-gray-500'>Profile</p>
+        <p className='text-2xl text-gray-500'>Profile Setting</p>
         <div className='border-t-[1px] border-gray-300/70 w-full my-2'></div>
         <div className='mx-auto my-4 flex flex-col gap-y-4 items-center justify-center'>
           <img
@@ -41,16 +41,22 @@ export default function SettingPage(props: TokenProp) {
             </div>
           </div>
         </div>
-        <div className='flex flex-col mb-6 w-1/2'>
-          <p className='uppercase text-gray-500 mb-4'>Contact Email</p>
-          <div className='flex flex-col'>
-            <div className='text-black '>asdfg09487@gmail.com</div>
-            <button className='border p-1 w-16 mt-4 rounded-md border-blue-300'>
-              Edit
-            </button>
+        <div className='flex flex-col mb-6 w-full items-start gap-4'>
+          <p className='uppercase text-gray-500 font-bold'>Contact Email</p>
+          <div className='flex flex-col md:flex-row md:items-center gap-4'>
+            <div className='text-black'>asdfg09487@gmail.com</div>
+            <div className='flex items-center'>
+              <div className='flex items-center gap-x-1 mr-4 bg-green-100 rounded-md p-1 px-2'>
+                <MdOutlineCheck fill='#16a34a' />
+                已驗證
+              </div>
+              <button className='border p-1 w-16 rounded-md border-blue-300'>
+                Edit
+              </button>
+            </div>
           </div>
         </div>
-        <div className='flex flex-col mb-6 w-1/2'>
+        {/* <div className='flex flex-col mb-6 w-1/2'>
           <p className='uppercase text-gray-500 mb-4'>Full Name</p>
           <div className='flex flex-col'>
             <div className='text-black '>asdfg09487@gmail.com</div>
@@ -67,7 +73,7 @@ export default function SettingPage(props: TokenProp) {
               Edit
             </button>
           </div>
-        </div>
+        </div> */}
       </div>
     </LayoutHome>
   )
