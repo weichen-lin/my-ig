80cb591a72b43570fdc0a844095173b6136a6c33
WeiChen Lin <asdfg09487@gmail.com>
2023-09-03 15:05:30 +0800

    :sparkles: Add gin

--- go-backend/db/user.sql_test.go
+++ go-backend/db/user.sql_test.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"testing"
 
+	"github.com/bxcodec/faker/v3"
 	"github.com/stretchr/testify/require"
 	"github.com/weichen-lin/myig/util"
 )
@@ -13,24 +14,35 @@ func Test_Createuser(t *testing.T) {
 
 	tx := NewTransaction(conn)
 
-	hashedPwd, err := util.HashPassword("123456")
+	fakePwd := faker.Password()[:10]
+
+	hashedPwd, err := util.HashPassword(fakePwd)
 	require.NoError(t, err)
 
 	arg := CreateUserParams{
-		Email:    "test@gmail.com",
+		Email:    faker.Email(),
 		Password: hashedPwd,
-		Name:     "test",
+		Name:     faker.Username(),
 	}
 
 	tx.ExecTx(context.Background(), func(tx *sql.Tx) error {
 		q := New(tx)
 
 		user, err := q.CreateUser(context.Background(), arg)
+		t.Log(user)
 		require.NoError(t, err)
 		require.NotEmpty(t, user)
-		err = util.ComparePassword(user.Password, "123456")
+
+		require.Equal(t, arg.Email, user.Email)
+		require.Equal(t, arg.Name, user.Name)
+
+		require.NotZero(t, user.ID)
+		require.NotEmpty(t, user.CreatedAt)
+		require.NotEmpty(t, user.LastModifiedAt)
+
+		err = util.ComparePassword(user.Password, fakePwd)
 		require.NoError(t, err)
 
 		return nil
-	}, true)
+	}, false)
 }
