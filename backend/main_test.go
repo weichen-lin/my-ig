c19ee9136371a81b2bfaee8fa2b6bc4a19c02b00
WeiChen Lin <asdfg09487@gmail.com>
2023-09-30 22:26:56 +0800

    :green_heart: Add test file at route

--- backend/main_test.go
+++ backend/main_test.go
@@ -1,10 +1,17 @@
 package main
 
-// func TestMain(m *testing.M) {
-// 	_, err := util.GetFirebase("test")
-// 	if err != nil {
-// 		panic(err)
-// 	}
+import (
+	"os"
+	"testing"
 
-// 	os.Exit(m.Run())
-// }
+	"github.com/weichen-lin/myig/util"
+)
+
+func TestMain(m *testing.M) {
+	_, err := util.GetFirebase("test")
+	if err != nil {
+		panic(err)
+	}
+
+	os.Exit(m.Run())
+}
