a0fd7b473b161c4659978dfe2881b866833a1868
WeiChen Lin <asdfg09487@gmail.com>
2023-10-03 23:54:42 +0800

    :recycle: Finish disk page

--- frontend/components/disk/files/index.tsx
+++ frontend/components/disk/files/index.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx'
-import Files from 'components/disk/files/files'
-import Folders from 'components/disk/files/folders'
+import { File } from 'components/disk/files/files'
+import { Folder } from 'components/disk/files/folders'
 import { KushareDriveBackbonePC } from './gdrivebone'
 import { useRecoilValue } from 'recoil'
 import { listMethodState, ListMethod, driveState, CommonProps } from 'store'
@@ -21,8 +21,22 @@ export default function KushareDrive() {
 
   if (isLoading) return <KushareDriveBackbonePC />
 
-  const files: CommonProps[] = [{ id: '1', name: 'test', last_modified_at: '2021-10-10' }]
-  const folders: CommonProps[] = [{ id: '1', name: 'test', last_modified_at: '2021-10-10' }]
+  const files: CommonProps[] = [
+    { id: '1', name: 'test', last_modified_at: '2021-10-10' },
+    { id: '1', name: 'test', last_modified_at: '2021-10-10' },
+    { id: '1', name: 'test', last_modified_at: '2021-10-10' },
+    { id: '1', name: 'test', last_modified_at: '2021-10-10' },
+    { id: '1', name: 'test', last_modified_at: '2021-10-10' },
+    { id: '1', name: 'test', last_modified_at: '2021-10-10' },
+  ]
+  const folders: CommonProps[] = [
+    { id: '1', name: 'test', last_modified_at: '2021-10-10' },
+    { id: '2', name: 'test', last_modified_at: '2021-10-10' },
+    { id: '3', name: 'test', last_modified_at: '2021-10-10' },
+    { id: '4', name: 'test', last_modified_at: '2021-10-10' },
+    { id: '5', name: 'test', last_modified_at: '2021-10-10' },
+    { id: '6', name: 'test', last_modified_at: '2021-10-10' },
+  ]
 
   return files.length > 0 && folders.length > 0 ? (
     <div
@@ -34,11 +48,19 @@ export default function KushareDrive() {
       {listMethod === ListMethod.Lattice && folders && folders.length > 0 && (
         <p className='mt-2 text-gray-400 xss:w-full xss:pl-[5%] xs:w-[20%] xs:pl-[1%]'>資料夾</p>
       )}
-      <Folders listMethod={listMethod} folders={folders} />
+      <div className='mx-auto flex w-full flex-col items-center gap-x-4 xs:flex-row xs:flex-wrap'>
+        {folders?.map((e: CommonProps) => (
+          <Folder info={e} method={listMethod} key={`folder_index_${e.id}`} />
+        ))}
+      </div>
       {listMethod === ListMethod.Lattice && files && files.length > 0 && (
         <p className='mt-2 pl-[1%] text-gray-400'>檔案</p>
       )}
-      <Files listMethod={listMethod} files={files} />
+      <div className='mx-auto flex w-full flex-col items-center gap-x-4 xs:flex-row xs:flex-wrap'>
+        {files?.map((e: CommonProps) => (
+          <File method={listMethod} info={e} key={`file_${e.id}`} />
+        ))}
+      </div>
     </div>
   ) : (
     <EmptyContent />
--- frontend/pages/home/index.tsx
+++ frontend/pages/home/index.tsx
@@ -1,8 +1,11 @@
-import { Sort, KushareDrive, Operator, ImagePlayground, UploadTasks, BreadCrumbs, Hinter } from 'components/disk'
+import { Sort, KushareDrive, Operator, BreadCrumbs, Hinter } from 'components/disk'
 import { LayoutHome } from 'components/layout'
 import { RecoilRoot, RecoilEnv } from 'recoil'
+import { GetServerSideProps } from 'next'
+import { CookieParser } from 'hooks/utils'
 
 RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false
+const cookieName = process.env.USER_AUTH_COOKIE_NAME ?? ''
 
 export default function Drive() {
   return (
@@ -19,4 +22,15 @@ export default function Drive() {
       </LayoutHome>
     </RecoilRoot>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const cookie = req.headers.cookie
+  const token = CookieParser({ cookie, name: cookieName })
+
+  return {
+    props: {
+      token,
+    },
+  }
 }
