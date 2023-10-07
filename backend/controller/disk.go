ec60b9976ffc12613a346f18833f0cd6a036fa01
WeiChen Lin <asdfg09487@gmail.com>
2023-10-07 21:51:16 +0800

    :bug: Close undone function

--- backend/controller/disk.go
+++ backend/controller/disk.go
@@ -1,35 +1,30 @@
 package controller
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
-	"github.com/weichen-lin/myig/db"
-	"github.com/weichen-lin/myig/util"
 )
 
 func (s *Controller) GetDisk(ctx *gin.Context) {
-	id_from_param := ctx.Param("f")
+	// id_from_param := ctx.Param("f")
 
-	folderId, err := util.ParseUUID(id_from_param)
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, errorResponse(ErrFolderIdInvalid))
-		return
-	}
+	// folderId, err := util.ParseUUID(id_from_param)
+	// if err != nil {
+	// 	ctx.JSON(http.StatusBadRequest, errorResponse(ErrFolderIdInvalid))
+	// 	return
+	// }
 
-	id_from_ctx := ctx.Value("userId").(string)
+	// id_from_ctx := ctx.Value("userId").(string)
 
-	userId, err := uuid.Parse(id_from_ctx)
-	if err != nil {
-		ctx.JSON(http.StatusUnauthorized, errorResponse(ErrAuthFailed))
-		return
-	}
+	// userId, err := uuid.Parse(id_from_ctx)
+	// if err != nil {
+	// 	ctx.JSON(http.StatusUnauthorized, errorResponse(ErrAuthFailed))
+	// 	return
+	// }
 
-	tx, err := s.Pool.Begin(ctx)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-		return
-	}
-	q := db.New(tx)
+	// tx, err := s.Pool.Begin(ctx)
+	// if err != nil {
+	// 	ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	// 	return
+	// }
+	// q := db.New(tx)
 }
