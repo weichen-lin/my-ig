e3febff25bc5353a28d2fd8be87a4b48ec4e4aca
WeiChen Lin <asdfg09487@gmail.com>
2023-10-24 18:02:39 +0800

    :sparkles: Finish history plugin

--- frontend/components/disk/editor/plugins/history.tsx
+++ frontend/components/disk/editor/plugins/history.tsx
@@ -1,23 +1,53 @@
-import { UNDO_COMMAND, REDO_COMMAND } from 'lexical'
+import { UNDO_COMMAND, REDO_COMMAND, CAN_REDO_COMMAND, COMMAND_PRIORITY_CRITICAL, CAN_UNDO_COMMAND } from 'lexical'
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext'
 import { Icon } from '@iconify/react'
 
+import { useEffect, useState } from 'react'
+
 export const History = () => {
   const [editor] = useLexicalComposerContext()
+  const [canUndo, setCanUndo] = useState(false)
+  const [canRedo, setCanRedo] = useState(false)
+
+  useEffect(() => {
+    editor.registerCommand(
+      CAN_REDO_COMMAND,
+      payload => {
+        setCanRedo(payload)
+        return payload
+      },
+      COMMAND_PRIORITY_CRITICAL,
+    )
+    editor.registerCommand(
+      CAN_UNDO_COMMAND,
+      payload => {
+        setCanUndo(payload)
+        return payload
+      },
+      COMMAND_PRIORITY_CRITICAL,
+    )
+  }, [])
+
   return (
     <div className='flex gap-x-1 px-1 items-center'>
-      <Icon
-        className='w-8 h-8 my-1 cursor-pointer hover:bg-slate-300/40 rounded-xl'
-        color='#929292'
-        icon='ei:undo'
-        onClick={() => editor.dispatchCommand(UNDO_COMMAND, undefined)}
-      />
-      <Icon
-        className='w-8 h-8 my-1 cursor-pointer hover:bg-slate-300/40 rounded-xl'
-        color='#929292'
-        icon='ei:redo'
-        onClick={() => editor.dispatchCommand(REDO_COMMAND, undefined)}
-      />
+      <button
+        disabled={!canUndo}
+        onClick={() => {
+          editor.dispatchCommand(UNDO_COMMAND, undefined)
+        }}
+        className='disabled:cursor-not-allowed disabled:opacity-30 cursor-pointer hover:bg-slate-300/40 rounded-xl'
+      >
+        <Icon className='w-8 h-8 my-1' color='#929292' icon='ei:undo' />
+      </button>
+      <button
+        disabled={!canRedo}
+        onClick={() => {
+          editor.dispatchCommand(REDO_COMMAND, undefined)
+        }}
+        className='disabled:cursor-not-allowed disabled:opacity-30 cursor-pointer hover:bg-slate-300/40 rounded-xl'
+      >
+        <Icon className='w-8 h-8 my-1' color='#929292' icon='ei:redo' />
+      </button>
     </div>
   )
 }
