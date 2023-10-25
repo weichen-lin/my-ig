e2aa00ccf0fe9c8b8019c8b840634d9471ff9711
WeiChen Lin <asdfg09487@gmail.com>
2023-10-25 13:35:33 +0800

    :sparkles: Finish heading styling

--- frontend/components/disk/editor/index.tsx
+++ frontend/components/disk/editor/index.tsx
@@ -1,17 +1,41 @@
 import { InitialConfigType, LexicalComposer } from '@lexical/react/LexicalComposer'
 import { PlainTextPlugin } from '@lexical/react/LexicalPlainTextPlugin'
+import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin'
 import { ContentEditable } from '@lexical/react/LexicalContentEditable'
 import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary'
-import { $getRoot, $getSelection } from 'lexical'
-import { useEffect } from 'react'
+import { $getRoot, $getSelection, EditorState } from 'lexical'
+import { useEffect, useState } from 'react'
 
 import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin'
-import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin'
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext'
-
+import Nodes from './node'
+import MyTheme from './theme'
 import Editor from './editor'
 
+interface OnChangePluginProps {
+  onChange: (editSatate: EditorState) => void
+}
+
+function OnChangePlugin({ onChange }: OnChangePluginProps) {
+  const [editor] = useLexicalComposerContext()
+  useEffect(() => {
+    return editor.registerUpdateListener(({ editorState }) => {
+      onChange(editorState)
+    })
+  }, [editor, onChange])
+  return null
+}
+
 export default function Description() {
+  const [editorState, setEditorState] = useState('')
+
+  function onChange(editorState: EditorState) {
+    // Call toJSON on the EditorState object, which produces a serialization safe string
+    const editorStateJSON = editorState.toJSON()
+    // However, we still have a JavaScript object, so we need to convert it to an actual string with JSON.stringify
+    setEditorState(JSON.stringify(editorStateJSON))
+  }
+
   const CustomContent = () => {
     return (
       <div className='flex flex-col h-full bg-white rounded-xl p-1'>
@@ -27,19 +51,22 @@ export default function Description() {
 
   const lexicalConfig: InitialConfigType = {
     namespace: 'My Rich Text Editor',
+    nodes: Nodes,
     onError: e => {
       console.log('ERROR:', e)
     },
+    theme: MyTheme,
   }
 
   return (
     <LexicalComposer initialConfig={lexicalConfig}>
-      <PlainTextPlugin
+      <RichTextPlugin
         contentEditable={<CustomContent />}
         placeholder={CustomPlaceholder}
         ErrorBoundary={LexicalErrorBoundary}
       />
       <HistoryPlugin />
+      <OnChangePlugin onChange={onChange} />
     </LexicalComposer>
   )
 }
--- frontend/components/disk/editor/plugins/paragraph.tsx
+++ frontend/components/disk/editor/plugins/paragraph.tsx
@@ -1,24 +1,75 @@
 import DropDownList from './dropdown'
 import { useState } from 'react'
+import { $getSelection, DEPRECATED_$isGridSelection, $isRangeSelection, $createParagraphNode } from 'lexical'
+import { $setBlocksType } from '@lexical/selection'
+import { $createHeadingNode, HeadingTagType } from '@lexical/rich-text'
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext'
+
+enum ParagraphType {
+  NORMAL = 'normal',
+  H1 = 'h1',
+  H2 = 'h2',
+  H3 = 'h3',
+  BULLET_LIST = 'bulletList',
+  NUMBERED_LIST = 'numberedList',
+  CHECK_LIST = 'checkList',
+  QUOTE = 'quote',
+  CODE_BLOCK = 'codeBlock',
+}
 
 const ParagraphOptions = [
-  { icon: 'subway:paragraph', title: 'Normal' },
-  { icon: 'material-symbols:format-h1-rounded', title: 'Heading 1' },
-  { icon: 'material-symbols:format-h2-rounded', title: 'Heading 2' },
-  { icon: 'material-symbols:format-h3-rounded', title: 'Heading 3' },
-  { icon: 'material-symbols:list', title: 'Bullet List' },
-  { icon: 'material-symbols:format-list-numbered', title: 'Numbered List' },
-  { icon: 'material-symbols:check-box-outline-rounded', title: 'Check List' },
-  { icon: 'ri:chat-quote-line', title: 'Quote' },
-  { icon: 'ph:code-block', title: 'Code Block' },
+  { icon: 'subway:paragraph', title: 'Normal', value: ParagraphType.NORMAL },
+  { icon: 'material-symbols:format-h1-rounded', title: 'Heading 1', value: ParagraphType.H1 },
+  { icon: 'material-symbols:format-h2-rounded', title: 'Heading 2', value: ParagraphType.H2 },
+  { icon: 'material-symbols:format-h3-rounded', title: 'Heading 3', value: ParagraphType.H3 },
+  { icon: 'material-symbols:list', title: 'Bullet List', value: ParagraphType.BULLET_LIST },
+  { icon: 'material-symbols:format-list-numbered', title: 'Numbered List', value: ParagraphType.NUMBERED_LIST },
+  { icon: 'material-symbols:check-box-outline-rounded', title: 'Check List', value: ParagraphType.CHECK_LIST },
+  { icon: 'ri:chat-quote-line', title: 'Quote', value: ParagraphType.QUOTE },
+  { icon: 'ph:code-block', title: 'Code Block', value: ParagraphType.CODE_BLOCK },
 ]
 
 export default function Paragraph() {
+  const [editor] = useLexicalComposerContext()
+
   const [currentOption, setCurrentOption] = useState(ParagraphOptions[0])
+
+  const formatParagraph = () => {
+    editor.update(() => {
+      const selection = $getSelection()
+      if ($isRangeSelection(selection) || DEPRECATED_$isGridSelection(selection)) {
+        $setBlocksType(selection, () => $createParagraphNode())
+      }
+    })
+  }
+
+  const formatHeading = (headingSize: HeadingTagType) => {
+    editor.update(() => {
+      const selection = $getSelection()
+      if ($isRangeSelection(selection) || DEPRECATED_$isGridSelection(selection)) {
+        $setBlocksType(selection, () => $createHeadingNode(headingSize))
+      }
+    })
+  }
 
   const handleSelect = (option: string) => {
     const index = ParagraphOptions.findIndex(item => item.title === option)
     setCurrentOption(ParagraphOptions[index])
+    const formatType = ParagraphOptions[index].value
+    // console.log({ formatType })
+    switch (formatType) {
+      case ParagraphType.NORMAL:
+        formatParagraph()
+        break
+      case ParagraphType.H1:
+      case ParagraphType.H2:
+      case ParagraphType.H3:
+        formatHeading(formatType)
+        break
+      default:
+        formatParagraph()
+        break
+    }
   }
 
   return (
