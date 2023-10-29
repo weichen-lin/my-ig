e72a1223ff79cd4c5bd20ce1020f3b9bf7e56338
WeiChen Lin <asdfg09487@gmail.com>
2023-10-30 00:15:48 +0800

    :bug: Fix auth method

--- backend/route/group.go
+++ backend/route/group.go
@@ -54,7 +54,7 @@ func PathRoute(r *gin.Engine) *gin.Engine {
 
 	file := r.Group("/file")
 	file.GET("/:id", ctl.AuthMiddlewareWithCookie(), ctl.GetFile)
-	file.GET("/description/:id", ctl.AuthMiddlewareWithCookie(), ctl.GetFileDescription)
+	file.GET("/description/:id", ctl.AuthMiddleware(), ctl.GetFileDescription)
 	file.POST("/create", ctl.AuthMiddleware(), ctl.CreateFile)
 	file.PATCH("/update", ctl.AuthMiddleware(), ctl.UpdateFileDescription)
 
