c7d13ad9ab1e728108f76908d2d0e58fa187c93f
WeiChen Lin <asdfg09487@gmail.com>
2023-11-03 16:21:06 +0800

    :sparkles: Adjust for limiting origin

--- backend/route/group.go
+++ backend/route/group.go
@@ -2,7 +2,6 @@ package route
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 	"os"
 	"strings"
@@ -19,7 +18,7 @@ func PathRoute(r *gin.Engine) *gin.Engine {
 	env := os.Getenv("KUSHARE_APP_ENV")
 
 	if env == "" {
-		env = "dev"
+		env = "test"
 	}
 
 	config, err := util.Loadconfig(".", env)
@@ -39,10 +38,8 @@ func PathRoute(r *gin.Engine) *gin.Engine {
 
 	ctl := controller.Controller{Pool: pool, SecretKey: config.SecretKey, BucketHandler: bucketHandler}
 
-	mode := controller.DevMode{IsDev: config.IsDev}
-	fmt.Println(mode)
-	if mode.IsDev {
-		r.Use(Cors())
+	if config.IsDev {
+		r.Use(Cors(config.AllowedDomain))
 	}
 
 	user := r.Group("/user")
@@ -72,39 +69,29 @@ func PathRoute(r *gin.Engine) *gin.Engine {
 	return r
 }
 
-// Cors 开放所有接口的OPTIONS方法
-func Cors() gin.HandlerFunc {
+func Cors(s string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		method := c.Request.Method               //请求方法
-		origin := c.Request.Header.Get("Origin") //请求头部
-		var headerKeys []string                  // 声明请求头keys
-		for k, _ := range c.Request.Header {
-			headerKeys = append(headerKeys, k)
-		}
-		headerStr := strings.Join(headerKeys, ", ")
-		if headerStr != "" {
-			headerStr = fmt.Sprintf("access-control-allow-origin, access-control-allow-headers, %s", headerStr)
-		} else {
-			headerStr = "access-control-allow-origin, access-control-allow-headers"
-		}
-		if origin != "" {
-			origin := c.Request.Header.Get("Origin")
-			c.Header("Access-Control-Allow-Origin", origin)                                            // 这是允许访问所有域
-			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PATCH, PUT, DELETE, UPDATE") //服务器支持的所有跨域请求的方法,为了避免浏览次请求的多次'预检'请求
-			//  header的类型
-			c.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token,session,X_Requested_With,Accept, Origin, Host, Connection, Accept-Encoding, Accept-Language,DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Pragma")
-			//              允许跨域设置                                                                                                      可以返回其他子段
-			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers,Cache-Control,Content-Language,Content-Type,Expires,Last-Modified,Pragma,FooBar") // 跨域关键设置 让浏览器可以解析
-			c.Header("Access-Control-Max-Age", "172800")                                                                                                                                                           // 缓存请求信息 单位为秒
-			c.Header("Access-Control-Allow-Credentials", "true")                                                                                                                                                   //  跨域请求是否需要带cookie信息 默认设置为true
-			c.Set("content-type", "application/json")                                                                                                                                                              // 设置返回格式是json
+		method := c.Request.Method
+		origin := c.Request.Header.Get("Origin")
+
+		if s != "" && !strings.HasSuffix(origin, s) {
+			c.String(http.StatusUnauthorized, "Invalid Authorization")
+			c.Abort()
+			return
 		}
 
-		//放行所有OPTIONS方法
+		c.Header("Access-Control-Allow-Origin", origin)
+		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PATCH, PUT, DELETE, UPDATE")
+
+		c.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token,session,X_Requested_With,Accept, Origin, Host, Connection, Accept-Encoding, Accept-Language,DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Pragma")
+		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers,Cache-Control,Content-Language,Content-Type,Expires,Last-Modified")
+		c.Header("Access-Control-Max-Age", "172800")
+		c.Header("Access-Control-Allow-Credentials", "true")
+
 		if method == "OPTIONS" {
 			c.JSON(http.StatusOK, "Options Request!")
 		}
-		// 处理请求
-		c.Next() //  处理请求
+
+		c.Next()
 	}
 }
