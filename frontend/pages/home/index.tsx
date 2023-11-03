96e477cdddc5d3f455af2bbe48d57d5f1b899506
WeiChen Lin <asdfg09487@gmail.com>
2023-11-03 16:59:22 +0800

    :construction_worker: Add frontend ci baseurl

--- frontend/pages/home/index.tsx
+++ frontend/pages/home/index.tsx
@@ -4,7 +4,6 @@ import { RecoilRoot, RecoilEnv } from 'recoil'
 import { GetServerSideProps } from 'next'
 import { CookieParser } from 'hooks/utils'
 import { ToastContainer } from 'react-toastify'
-import { ImagePlayground } from 'components/disk/'
 import 'react-toastify/dist/ReactToastify.css'
 
 RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false
