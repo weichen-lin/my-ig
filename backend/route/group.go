ddf3bd30416e30af2451e394f2c5f3d1464fe599
WeiChen Lin <asdfg09487@gmail.com>
2023-11-04 18:11:44 +0800

    :bug: Expose header and adjust cookie domain

--- backend/route/group.go
+++ backend/route/group.go
@@ -84,7 +84,7 @@ func Cors(s string) gin.HandlerFunc {
 		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PATCH, PUT, DELETE, UPDATE")
 
 		c.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token,session,X_Requested_With,Accept, Origin, Host, Connection, Accept-Encoding, Accept-Language,DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Pragma")
-		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Access-Control-Allow-Methods, Cache-Control,Content-Language,Content-Type,Expires,Last-Modified,Pragma")
+		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Access-Control-Allow-Methods, Cache-Control,Content-Language,Content-Type,Expires,Last-Modified,Pragma, Set-Cookie")
 		c.Header("Access-Control-Max-Age", "172800")
 		c.Header("Access-Control-Allow-Credentials", "true")
 
