69fcc17e095b42509c1cf485204cea1e39fedca9
WeiChen Lin <asdfg09487@gmail.com>
2023-11-05 22:50:13 +0800

    :bug: Remove cookie expire

--- backend/controller/user.go
+++ backend/controller/user.go
@@ -113,7 +113,7 @@ func (s *Controller) UserRegister(ctx *gin.Context) {
 		return
 	}
 
-	ctx.Header("Set-Cookie", fmt.Sprintf("%s=%s; Path=/; Max-Age=3600; HttpOnly; Secure; SameSite=None", userTokenName, token))
+	ctx.Header("Set-Cookie", fmt.Sprintf("%s=%s; Path=/; HttpOnly; Secure; SameSite=None", userTokenName, token))
 	ctx.String(http.StatusOK, user.ID.String())
 }
 
@@ -157,7 +157,7 @@ func (s *Controller) UserLogin(ctx *gin.Context) {
 		return
 	}
 
-	ctx.Header("Set-Cookie", fmt.Sprintf("%s=%s; Path=/; Max-Age=3600; HttpOnly; Secure; SameSite=None", userTokenName, token))
+	ctx.Header("Set-Cookie", fmt.Sprintf("%s=%s; Path=/; HttpOnly; Secure; SameSite=None", userTokenName, token))
 	ctx.JSON(http.StatusOK, info.ID)
 }
 
