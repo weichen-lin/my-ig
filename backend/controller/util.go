46a145d56ad33c8ba0d04ba99e6add21b70f0608
WeiChen Lin <asdfg09487@gmail.com>
2023-11-09 18:12:37 +0800

    :recycle: Refactor env

--- backend/controller/util.go
+++ backend/controller/util.go
@@ -9,10 +9,17 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+type EmailSetting struct {
+	SecretKey string
+	AppPassword string
+	EncryptSecret string
+}
+
 type Controller struct {
 	Pool          *pgxpool.Pool
 	SecretKey     string
 	BucketHandler *storage.BucketHandle
+	AppPassword   string
 	EncryptSecret string
 }
 
