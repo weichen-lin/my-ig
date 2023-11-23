bcf7f8e885e74ab87a738eb1337fa4bf234dcf5e
WeiChen Lin <asdfg09487@gmail.com>
2023-11-23 23:17:15 +0800

    :sparkles: Add my drag package

--- frontend/components/disk/selectable/index.tsx
+++ frontend/components/disk/selectable/index.tsx
@@ -1,40 +1,40 @@
 import { useEffect, useMemo, useRef } from 'react'
-// import { useDrag } from 'hooks/disk'
-// import Selectable, { DragStatus } from 'selection'
+import { useDrag } from 'hooks/disk'
+import Selectable, { DragStatus } from 'gdrive-select-and-drag'
 
 export default function SelectArea(props: { children: JSX.Element }) {
   const { children } = props
-  // const { startDrag, endDrag, selectDisk } = useDrag()
+  const { startDrag, endDrag, selectDisk } = useDrag()
   const ref = useRef<HTMLDivElement>(null)
 
-  // const selectable = useMemo(() => {
-  //   return new Selectable({
-  //     canStartSelect: true,
-  //     selectAreaClassName: 'selection-area',
-  //     selectablePrefix: 'selectable',
-  //     select_cb: selected => {
-  //       selectDisk(selected)
-  //     },
-  //   })
-  // }, [])
+  const selectable = useMemo(() => {
+    return new Selectable({
+      canStartSelect: true,
+      selectAreaClassName: 'selection-area',
+      selectablePrefix: 'selectable',
+      select_cb: selected => {
+        selectDisk(selected)
+      },
+    })
+  }, [])
 
-  // const selectRef = useRef(selectable).current
+  const selectRef = useRef(selectable).current
 
-  // useEffect(() => {
-  //   if (ref?.current && selectable.selectBoundary !== ref?.current) {
-  //     selectRef.init(ref?.current)
-  //   }
+  useEffect(() => {
+    if (ref?.current && selectable.selectBoundary !== ref?.current) {
+      selectRef.init(ref?.current)
+    }
 
-  //   selectable.drag_cb = (stored, status, dragOnEle) => {
-  //     if (status === DragStatus.Start) {
-  //       startDrag()
-  //     }
+    selectable.drag_cb = (stored, status, dragOnEle) => {
+      if (status === DragStatus.Start) {
+        startDrag()
+      }
 
-  //     if (status === DragStatus.End) {
-  //       endDrag({ stored, dragOnEle })
-  //     }
-  //   }
-  // }, [startDrag, endDrag, ref?.current])
+      if (status === DragStatus.End) {
+        endDrag({ stored, dragOnEle })
+      }
+    }
+  }, [startDrag, endDrag, ref?.current])
 
   return <div ref={ref}>{children}</div>
 }
