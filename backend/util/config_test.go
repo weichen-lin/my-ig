31e6be189399fa3e50b08d3af7049ebb14111c29
WeiChen Lin <asdfg09487@gmail.com>
2024-01-26 17:26:00 +0800

    :fire: Fix config and stop one flow

--- backend/util/config_test.go
+++ backend/util/config_test.go
@@ -12,7 +12,7 @@ func Test_LoadConfig(t *testing.T) {
 
 	require.Equal(t, "postgres", config.DBDriver)
 	require.Equal(t, "postgresql://myigroot:myigrootpwd@localhost:5432/development?sslmode=disable", config.DBSource)
-	require.Equal(t, "0.0.0.0:8000", config.ServerAddress)
+	require.Equal(t, "0.0.0.0:8080", config.ServerAddress)
 	require.Equal(t, "AAAAAAAA____secretkeyfortest____AAAAAAAA", config.SecretKey)
 	require.Equal(t, "kushare-7abab.appspot.com", config.FireBaseBucket)
 }
