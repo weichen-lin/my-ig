d4acb887ab2f31c878c2902af8b35d9702b3b3cf
WeiChen Lin <asdfg09487@gmail.com>
2024-01-26 17:23:21 +0800

    :fire: Stop push image to render

--- backend/util/config_test.go
+++ backend/util/config_test.go
@@ -11,7 +11,7 @@ func Test_LoadConfig(t *testing.T) {
 	require.NoError(t, err)
 
 	require.Equal(t, "postgres", config.DBDriver)
-	require.Equal(t, "postgresql://root:test_local@localhost:5432/myig?sslmode=disable", config.DBSource)
+	require.Equal(t, "postgresql://myigroot:myigrootpwd@localhost:5432/development?sslmode=disable", config.DBSource)
 	require.Equal(t, "0.0.0.0:8000", config.ServerAddress)
 	require.Equal(t, "AAAAAAAA____secretkeyfortest____AAAAAAAA", config.SecretKey)
 	require.Equal(t, "kushare-7abab.appspot.com", config.FireBaseBucket)
